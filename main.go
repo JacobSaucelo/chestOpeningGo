1fce61aa52f8317799d8c0e9a9f4100d622689d6
Jacob Saucelo <jacobsaucelo32@gmail.com>
2023-12-08 15:09:26 +0800

    final commit
    found a better way

--- main.go
+++ main.go
@@ -52,58 +52,32 @@ var hats = []Hat{
 	},
 }
 
-var caseItems = []CaseItem{
-	{Name: "------------------AWP | Dragon Lore", Rarity: 0.0025},
-	{Name: "AK-47 | Fire Serpent", Rarity: 0.249375},
-	{Name: "M4A4 | Howl", Rarity: 0.249375},
-	{Name: "Glock-18 | Fade", Rarity: 0.249375},
-	{Name: "USP-S | Kill Confirmed", Rarity: 0.249375},
-	{Name: "Desert Eagle | Golden Koi", Rarity: 0.249375},
-}
-
-type CaseItem struct {
-	Name   string
-	Rarity float64
-}
-
 func main() {
-	// caseHardened := Chest{
-	// 	Hats: hats,
-	// }
-	// totalPercentage := 0.0025 + 0.249375 + 0.249375 + 0.249375 + 0.249375
-	totalPercentage := 0.0
-
-	for _, perc := range caseItems {
-		totalPercentage += perc.Rarity
+	chest1 := Chest{
+		Hats: hats,
 	}
 
-	fmt.Println("totalPercentage: ", totalPercentage)
-	// for i := 0; i < 10; i++ {
-	// }
-	// fmt.Println(randomNumber)
-	cumulativePercentage := 100.0
-	// for i := 0; i < 100; i++ {
-	tries := 0
-	for cumulativePercentage > 0.249375 {
-		tries += 1
-		randomNumber := rand.Float64() * totalPercentage
-		cumulativePercentage = 0.0
-		for _, item := range caseItems {
-			cumulativePercentage += item.Rarity
-			if randomNumber <= cumulativePercentage {
-				// fmt.Println("randomNumber: ", randomNumber)
-				// fmt.Println("cumulativePercentage: ", cumulativePercentage)
-				fmt.Println("tries: ", tries, "Item: ", item, "cumulative: ", cumulativePercentage, "randomNumber:", randomNumber)
-				break
-			}
+	chest1.OpenChest()
+}
+
+func (c *Chest) OpenChest() {
+	totalPercentage := 0.0
+
+	for _, perc := range c.Hats {
+		totalPercentage += float64(perc.Rarity)
+	}
+
+	randomNumber := rand.Float64() * totalPercentage
+
+	cumulativePercentage := 0.0
+
+	for _, item := range c.Hats {
+		cumulativePercentage += float64(item.Rarity)
+		if randomNumber <= cumulativePercentage {
+			// fmt.Println("randomNumber: ", randomNumber)
+			// fmt.Println("cumulativePercentage: ", cumulativePercentage)
+			fmt.Println("Item: ", item, "cumulative: ", cumulativePercentage, "randomNumber:", randomNumber)
+			break
 		}
 	}
 }
-
-// func (c *Chest) OpenChest() {
-// 	totalProbability := 0.0
-
-// 	for _, hat := range c.Hats {
-// 		totalProbability += hat.Rarity
-// 	}
-// }
